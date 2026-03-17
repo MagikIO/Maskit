0a490f044e756be403f4b1793463b1bb4d8a28d0
Antonio Bourassa <Abourassa@AssetVal.com>
2026-03-17 12:35:09 -0600

    feat: update Vitest configuration to support TypeScript and TSX test files

--- packages/solid/vitest.config.ts
+++ packages/solid/vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vitest/config";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  plugins: [solidPlugin()],
   test: {
     name: "solid",
     environment: "jsdom",
-    include: ["test/**/*.test.ts"],
+    include: ["test/**/*.test.{ts,tsx}"],
   },
 });
